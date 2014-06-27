24091ede95134a0c788cadb55721a5f7bfa17f37
rishiban91 <rishiban_1991@yahoo.com>
2014-06-26 21:21:51 -0400

    mistkaes rectified

--- src/main/java/com/farmfinder/services/PaymentService.java
+++ src/main/java/com/farmfinder/services/PaymentService.java
@@ -3,16 +3,16 @@ package com.farmfinder.services;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
-<<<<<<< HEAD
+
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-=======
+
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
->>>>>>> 11fa7ec3bbaa63df47eab80281ee93659d6ee4b4
+
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -26,16 +26,8 @@ import com.farmfinder.commands.PaymentRepo;
 import com.farmfinder.config.MongoConfig;
 import com.farmfinder.model.Category;
 import com.farmfinder.model.Payment;
-<<<<<<< HEAD
-import com.sun.xml.bind.v2.model.core.ID;
 
-public class PaymentService {
-	@POST
-	@Path("/createPayment")
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response createPayment(String data) {
-=======
+import com.sun.xml.bind.v2.model.core.ID;
 
 public class PaymentService {
 	@POST
@@ -43,45 +35,41 @@ public class PaymentService {
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response createPayment(String data){
->>>>>>> 11fa7ec3bbaa63df47eab80281ee93659d6ee4b4
+
 		/* We are getting application context and repo here*/
 		ApplicationContext ctx = new AnnotationConfigApplicationContext(MongoConfig.class);
 		PaymentRepo repo = (PaymentRepo) ctx.getBean(PaymentRepo.class) ;
 		/* here we convert Payment class to match JSON*/
 		ObjectMapper map = new ObjectMapper();
-<<<<<<< HEAD
+
 		try { 
 			Payment p = map.readValue(data, Payment.class) ;
 		}
 		catch (Exception e) {
-=======
+			e.printStackTrace();
+		}
 		try{ 
 			Payment p = map.readValue(data, Payment.class) ;
-		}catch (Exception e){
->>>>>>> 11fa7ec3bbaa63df47eab80281ee93659d6ee4b4
-			e.printStackTrace();
+		}catch (Exception e1) {
+			e1.printStackTrace();
 		}
 		
 		/*Create new payment class*/ 
 		Payment pay = new Payment() ;
 		pay.setName("Buy your products") ;		
 		repo.save(pay) ;
-		return Response.status(201).entity(pay).build() ;
-<<<<<<< HEAD
-	  }
+		return Response.status(201).entity(pay).build();
+		}
+	 
 	@GET
 	@Path("/geAllPayment")
 	@Produces(MediaType.APPLICATION_JSON)
-=======
-	}
->>>>>>> 11fa7ec3bbaa63df47eab80281ee93659d6ee4b4
 	public Response getAllPayment() {
 		/* We are getting application context and repo here*/
 		ApplicationContext ctx = new AnnotationConfigApplicationContext(MongoConfig.class);
 		PaymentRepo repo = (PaymentRepo) ctx.getBean(PaymentRepo.class) ;
 		List<Payment> getpay = repo.findAll();
 		return Response.status(201).entity(getpay).build();
-<<<<<<< HEAD
 	}
 	/*rest service for find all payments*/
 	@GET
@@ -105,12 +93,12 @@ public class PaymentService {
 		return Response.status(200).entity(id).build();
 	}
 	
-}	
-=======
-		}
-	}
 	
->>>>>>> 11fa7ec3bbaa63df47eab80281ee93659d6ee4b4
+
+		}
+	
+	
+
 	
 	
 	
